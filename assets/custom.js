1e08197d7a6aa5045b66f40637f68db1b920227a
Phermidex <hermides07@hmail.com>
2021-05-24 15:42:53 -0400

    my first commit

---
+++ assets/custom.js
@@ -0,0 +1,285 @@
+// Blog Buy button - Different Bikini Wax Styles to Know in 2020
+
+// (function () {
+//   var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+//   if (window.ShopifyBuy) {
+//     if (window.ShopifyBuy.UI) {
+//       ShopifyBuyInit();
+//     } else {
+//       loadScript();
+//     }
+//   } else {
+//     loadScript();
+//   }
+//   function loadScript() {
+//     var script = document.createElement('script');
+//     script.async = true;
+//     script.src = scriptURL;
+//     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+//     script.onload = ShopifyBuyInit;
+//   }
+//   function ShopifyBuyInit() {
+//     var client = ShopifyBuy.buildClient({
+//       domain: 'nacach.myshopify.com',
+//       storefrontAccessToken: '805b0cd2062073e3cf5706865e7fbd85',
+//     });
+//     ShopifyBuy.UI.onReady(client).then(function (ui) {
+//       ui.createComponent('product', {
+//         id: '4536023023754',
+//         node: document.getElementById('product-component-1604495463812'),
+//         moneyFormat: '%24%7B%7Bamount%7D%7D',
+//         options: {
+//   "product": {
+//     "styles": {
+//       "product": {
+//         "@media (min-width: 601px)": {
+//           "max-width": "calc(25% - 20px)",
+//           "margin-left": "20px",
+//           "margin-bottom": "50px"
+//         }
+//       },
+//       "button": {
+//         "font-family": "Montserrat, sans-serif",
+//         ":hover": {
+//           "background-color": "#000000"
+//         },
+//         "background-color": "#000000",
+//         ":focus": {
+//           "background-color": "#000000"
+//         }
+//       }
+//     },
+//     "text": {
+//       "button": "Add to cart"
+//     },
+//     "googleFonts": [
+//       "Montserrat"
+//     ]
+//   },
+//   "productSet": {
+//     "styles": {
+//       "products": {
+//         "@media (min-width: 601px)": {
+//           "margin-left": "-20px"
+//         }
+//       }
+//     }
+//   },
+//   "modalProduct": {
+//     "contents": {
+//       "img": false,
+//       "imgWithCarousel": true,
+//       "button": false,
+//       "buttonWithQuantity": true
+//     },
+//     "styles": {
+//       "product": {
+//         "@media (min-width: 601px)": {
+//           "max-width": "100%",
+//           "margin-left": "0px",
+//           "margin-bottom": "0px"
+//         }
+//       },
+//       "button": {
+//         "font-family": "Montserrat, sans-serif",
+//         ":hover": {
+//           "background-color": "#000000"
+//         },
+//         "background-color": "#000000",
+//         ":focus": {
+//           "background-color": "#000000"
+//         }
+//       }
+//     },
+//     "googleFonts": [
+//       "Montserrat"
+//     ],
+//     "text": {
+//       "button": "Add to cart"
+//     }
+//   },
+//   "cart": {
+//     "styles": {
+//       "button": {
+//         "font-family": "Montserrat, sans-serif",
+//         ":hover": {
+//           "background-color": "#000000"
+//         },
+//         "background-color": "#000000",
+//         ":focus": {
+//           "background-color": "#000000"
+//         }
+//       }
+//     },
+//     "text": {
+//       "total": "Subtotal",
+//       "button": "Checkout"
+//     },
+//     "googleFonts": [
+//       "Montserrat"
+//     ]
+//   },
+//   "toggle": {
+//     "styles": {
+//       "toggle": {
+//         "font-family": "Montserrat, sans-serif",
+//         "background-color": "#000000",
+//         ":hover": {
+//           "background-color": "#000000"
+//         },
+//         ":focus": {
+//           "background-color": "#000000"
+//         }
+//       }
+//     },
+//     "googleFonts": [
+//       "Montserrat"
+//     ]
+//   }
+// },
+//       });
+//     });
+//   }
+// })();
+
+// (function () {
+//   var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+//   if (window.ShopifyBuy) {
+//     if (window.ShopifyBuy.UI) {
+//       ShopifyBuyInit();
+//     } else {
+//       loadScript();
+//     }
+//   } else {
+//     loadScript();
+//   }
+//   function loadScript() {
+//     var script = document.createElement('script');
+//     script.async = true;
+//     script.src = scriptURL;
+//     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+//     script.onload = ShopifyBuyInit;
+//   }
+//   function ShopifyBuyInit() {
+//     var client = ShopifyBuy.buildClient({
+//       domain: 'nacach.myshopify.com',
+//       storefrontAccessToken: '805b0cd2062073e3cf5706865e7fbd85',
+//     });
+//     ShopifyBuy.UI.onReady(client).then(function (ui) {
+//       ui.createComponent('product', {
+//         id: '4536022761610',
+//         node: document.getElementById('product-component-1604496107526'),
+//         moneyFormat: '%24%7B%7Bamount%7D%7D',
+//         options: {
+//   "product": {
+//     "styles": {
+//       "product": {
+//         "@media (min-width: 601px)": {
+//           "max-width": "calc(25% - 20px)",
+//           "margin-left": "20px",
+//           "margin-bottom": "50px"
+//         }
+//       },
+//       "button": {
+//         "font-family": "Montserrat, sans-serif",
+//         ":hover": {
+//           "background-color": "#000000"
+//         },
+//         "background-color": "#000000",
+//         ":focus": {
+//           "background-color": "#000000"
+//         }
+//       }
+//     },
+//     "text": {
+//       "button": "Add to cart"
+//     },
+//     "googleFonts": [
+//       "Montserrat"
+//     ]
+//   },
+//   "productSet": {
+//     "styles": {
+//       "products": {
+//         "@media (min-width: 601px)": {
+//           "margin-left": "-20px"
+//         }
+//       }
+//     }
+//   },
+//   "modalProduct": {
+//     "contents": {
+//       "img": false,
+//       "imgWithCarousel": true,
+//       "button": false,
+//       "buttonWithQuantity": true
+//     },
+//     "styles": {
+//       "product": {
+//         "@media (min-width: 601px)": {
+//           "max-width": "100%",
+//           "margin-left": "0px",
+//           "margin-bottom": "0px"
+//         }
+//       },
+//       "button": {
+//         "font-family": "Montserrat, sans-serif",
+//         ":hover": {
+//           "background-color": "#000000"
+//         },
+//         "background-color": "#000000",
+//         ":focus": {
+//           "background-color": "#000000"
+//         }
+//       }
+//     },
+//     "googleFonts": [
+//       "Montserrat"
+//     ],
+//     "text": {
+//       "button": "Add to cart"
+//     }
+//   },
+//   "cart": {
+//     "styles": {
+//       "button": {
+//         "font-family": "Montserrat, sans-serif",
+//         ":hover": {
+//           "background-color": "#000000"
+//         },
+//         "background-color": "#000000",
+//         ":focus": {
+//           "background-color": "#000000"
+//         }
+//       }
+//     },
+//     "text": {
+//       "total": "Subtotal",
+//       "button": "Checkout"
+//     },
+//     "googleFonts": [
+//       "Montserrat"
+//     ]
+//   },
+//   "toggle": {
+//     "styles": {
+//       "toggle": {
+//         "font-family": "Montserrat, sans-serif",
+//         "background-color": "#000000",
+//         ":hover": {
+//           "background-color": "#000000"
+//         },
+//         ":focus": {
+//           "background-color": "#000000"
+//         }
+//       }
+//     },
+//     "googleFonts": [
+//       "Montserrat"
+//     ]
+//   }
+// },
+//       });
+//     });
+//   }
+// })();
